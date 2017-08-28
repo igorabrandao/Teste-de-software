d3840a91b4ce713719ac69a7226759de6b2c44b0
Igor Brandão <igorabrandao@gmail.com>
2017-08-28 16:59:37 -0300

    Implement the check-in/check-out input and date diff

--- ParkingLotCalculator/src/main/java/imd0412/parkinglot/calculator/Calculator.java
+++ ParkingLotCalculator/src/main/java/imd0412/parkinglot/calculator/Calculator.java
@@ -1,6 +1,14 @@
 package imd0412.parkinglot.calculator;
 
+import static org.apache.commons.lang3.time.DurationFormatUtils.formatDuration;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.regex.Matcher;
+
+import imd0412.parkinglot.Constants;
 import imd0412.parkinglot.ParkingLotType;
+import imd0412.parkinglot.exception.*;
 
 public class Calculator {
 	/**
@@ -11,7 +19,96 @@ public class Calculator {
 	 * @param type
 	 * @return
 	 */
-	Float calculateParkingCost(String checkin, String checkout, ParkingLotType type) {
-		throw new IllegalStateException("Not yet implemented.");
+	Float calculateParkingCost(String checkin, String checkout, ParkingLotType type) throws DateFormatException {
+		
+		// Local variables
+		Date check_in = null;
+		Date check_out = null;
+		
+		// Diff variables
+		String diff_year = "";
+		String diff_month = "";
+		String diff_day = "";
+		String diff_hour = "";
+		String diff_minute = "";
+		
+		// Calculated price
+		float calculated_price = 0;
+		
+		// ==========================================================================================
+		// DATES INPUT HANDLER
+		// ==========================================================================================
+		
+		// Try to converts the check-in to a java.util.Date
+		try {
+			check_in = Constants.DATE_FORMATTER.parse(checkin);
+		} catch (ParseException e) {
+			// Message to the user
+			String errorMsg = "\nString " + checkin
+					+ " está fora do padrão.";
+			System.out.println(errorMsg);
+			
+			// Throws the custom exception
+			throw new DateFormatException(errorMsg);
+		}
+		
+		// Try to converts the check-out to a java.util.Date
+		try {
+			check_out = Constants.DATE_FORMATTER.parse(checkout);
+		} catch (ParseException e) {
+			// Message to the user
+			String errorMsg = "\nString " + check_out
+					+ " está fora do padrão.";
+			System.out.println(errorMsg);
+			
+			// Throws the custom exception
+			throw new DateFormatException(errorMsg);
+		}
+		
+		// ==========================================================================================
+		// DATE DIFFERENCE CALCULATOR HANDLER
+		// ==========================================================================================
+		
+		// Calculates the difference in milliseconds
+		long diff = check_out.getTime() - check_in.getTime();
+
+		// Converts the difference into date type
+		String duration = formatDuration(diff, Constants.VALID_DATE_FORMAT);
+		
+		// Split the difference into sections
+		Matcher matcher = Constants.DATE_PATTERN.matcher(duration);
+		if (matcher.find()) {
+			diff_year = matcher.group(1);
+			diff_month = matcher.group(2);
+			diff_day = matcher.group(3);
+			diff_hour = matcher.group(4);
+			diff_minute = matcher.group(5);
+		}
+		
+		// ==========================================================================================
+		// Calculate the price for each use type
+		// ==========================================================================================
+		
+		// Check the use type
+		switch (type) {
+			// ==========================================================================================
+			// (I) ShortTerm: use just for a few hours
+			// ==========================================================================================
+			case ShortTerm:
+				break;
+			// ==========================================================================================
+			// (II) LongTerm: use for more than few hours, maybe some days
+			// ==========================================================================================
+			case LongTerm:
+				break;
+			// ==========================================================================================
+			// (III) VIP: for whom looking for comfort and security
+			// ==========================================================================================
+			case VIP:
+				break;
+		}
+		
+		// Return the calculated price
+		return calculated_price;
 	}
 }
